ec9983489b8a71e7f885f9a1663778181e8f2e14
aizarashid16 <aizarashid16@gmail.com>
2024-06-15 15:37:06 +0500

    'sec'

--- userauth/authenapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from authenapp import views
-
-app_name="authenapp"
-urlpatterns=[
-    path("register/",views.register,name="register"),
-    path("user_login/",views.user_login,name="user_login"),
-]
